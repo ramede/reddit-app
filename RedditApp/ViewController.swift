5ad0c686cda8155b0b01754ec37739f9dbdd4d20
Râmede Bento <ramede.pereira@willbank.com.br>
2021-08-10 20:28:43 -0300

    Initial Commit

---
+++ RedditApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RedditApp
+//
+//  Created by Râmede on 10/08/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

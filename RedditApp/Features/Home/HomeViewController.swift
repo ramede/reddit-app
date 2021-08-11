22e681a2bd0a1770b84b61b43e7139051a19abab
Râmede Bento <ramede.pereira@willbank.com.br>
2021-08-10 21:29:01 -0300

    Creating a uikit foundation project and custom lauchscreen

---
+++ RedditApp/Features/Home/HomeViewController.swift
@@ -0,0 +1,20 @@
+//
+//  HomeViewController.swift
+//  RedditApp
+//
+//  Created by Râmede on 10/08/21.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(true)
+        view.backgroundColor = .red
+    }
+}
